cb3e1792f262f8178075ce942d42a5b0e5abd297
vungoctan1109 <vntan1109@gmail.com>
2022-05-07 16:49:11 +0700

    add client view

--- EAD_Assignment/Controllers/HomeController.cs
+++ EAD_Assignment/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using System.Net;
 
 namespace EAD_Assignment.Controllers
 {
@@ -15,15 +17,44 @@ namespace EAD_Assignment.Controllers
 
         public ActionResult Index()
         {
-            var Articles = (from article in db.Articles select article).ToList();
-            return View(Articles);
+            var articles = from s in db.Articles select s;
+            articles = articles.OrderBy(s => s.CreatedAt);
+            ViewBag.CategoryList = from s in db.Categories select s;
+            return View(articles.ToList());
         }
 
         [HttpPost]
         public ActionResult Index(Article article)
         {
             var data = db.Articles.ToList();
+            ViewBag.CategoryList = from s in db.Categories select s;
             return View(data);
+        }
+
+        public ActionResult SearchByCategory(int category, int? page)
+        {
+            var articles = from s in db.Articles where (s.CategoryId == category) orderby (s.CreatedAt) select s;
+            ViewBag.CategoryList = from s in db.Categories select s;
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(articles.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult Details(string url)
+        {
+            if (url == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(url);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryList = from s in db.Categories select s;
+            ViewBag.RelatedPost = from s in db.Articles where (s.CategoryId == article.CategoryId) select s;
+
+            return View(article);
         }
 
         public ActionResult About()
